d768c465bf6bc3ef66fc44ee4a3c32f02b720655
T-lorrain <72496171+T-lorrain@users.noreply.github.com>
2021-01-06 10:01:59 +0100

    Update maison

--- maison.py
+++ maison.py
@@ -1,24 +1,49 @@
 import sys
+import os
 import sysv_ipc
 
 
-
+keyMarket = 999
+keyHome = 777
 
 if __name__ == "__main__":
+
     InitProd = int(sys.argv[1])
     ConsoRate = int(sys.argv[2])
     SalePol = int(sys.argv[3]) # 0 pour Toujours Donner, 1 pour Toujours Vendre, 2 pour Vendre si personne prend
+    pid = os.getpid()
+
+    try:
+        mqmarket = sysv_ipc.MessageQueue(keyMarket)
+    except ExistentialError:
+        print("Message queue", keyMarket, "already exsits, terminating.")
+        sys.exit(1)
+
+    try:
+        mqhome = sysv_ipc.MessageQueue(keyHome, sysv_ipc.IPC_CREX)
+    except ExistentialError:
+        print("Message queue", keyHome, "already exsits, terminating.")
+        sys.exit(1)
+
+
 
     if ConsoRate > InitProd:
         Quantity = ConsoRate - InitProd
+        m1 = str(pid), ",", str(Quantity)
+        m2 = m1.encode()
+        mqhome.send(m2)
+
+
 
     elif ConsoRate < InitProd:
         if SalePol == 0:
-            # Attendre Message dans MQ entre Home
+            m, t = mqhome.receive()
+            print(t)
+            """
         elif SalePol == 1:
-            # Envoyer Message dans MQ vers Market
+            #Envoyer Message dans MQ vers Market
         elif SalePol == 2:
-            # Attendre Demande pendant X sec, si pas de réponse alors envoyer
-
+            #Attendre Demande pendant X sec, si pas de réponse alors envoyer
+"""
 
 
