bba33f6abe61611559745da134a0762ebf0e929a
T-lorrain <72496171+T-lorrain@users.noreply.github.com>
2021-01-18 17:15:39 +0100

    Commentaires Maison

--- maison.py
+++ maison.py
@@ -5,44 +5,57 @@ import multiprocessing
 import random
 import time
 
-
+# Clés des 2 Messages Queues
 keyMarket = 999
 keyHome = 777
 
-
+#Processus Maison
 def maison(InitProd, ConsoRate, SalePol, mqhome, mqmarket):
     i = 1
 
     while True:
+        #Permet de faire en sorte qu'un seul processus maison affiche le numéro du Tour avant de passer à l'execution des transactions
         cond = b.wait()
         if cond == 0:
             print("Tour ", i)
         b.wait()
+
+        #Affichage des caractéristiques de la Maison
         pid = os.getpid()
         print("Maison ", pid, " | Consommation : ", ConsoRate, " | Production : ", InitProd, "\n")
 
-        if ConsoRate > InitProd:  # Implémenter un boucle pour accéder à retour à la normale (tant que j'ai pas recu le bon message, récupérez des messages)
+        #Si la Consommation est supérieur à la production, on envoie une demande dans la Message Queue entre les maisons
+        if ConsoRate > InitProd:
             Quantity = ConsoRate - InitProd
             m1 = "%d,%d" % (pid, Quantity)
             m2 = m1.encode()
+            #envoi de la demande
             mqhome.send(m2, type=2)
 
             try:
+                #Attente d'une réponse d'une maison
                 time.sleep(5)
                 rep, t = mqhome.receive(type=pid, block=False)
                 print(rep.decode())
+            #Si pas de réponse
             except sysv_ipc.BusyError:
                 m = "%d,%d" % (pid, -Quantity)
                 m = m.encode()
+                #envoi d'une demande d'achat au market
                 mqmarket.send(m, type=1)
+                #Réception de l'ACK, si pas d'ACK, simulation bloquée
                 m, t = mqmarket.receive(type=pid)
                 #print("m2 is ", m, "\n")
 
+        #Cas de surplus d'énergie
         elif ConsoRate < InitProd:
             surplus = InitProd - ConsoRate
+            #Cas Toujours Donner le surplus
             if SalePol == 0:
                 try:
+                    #Attente d'une demande
                     time.sleep(2)
+                    #Réception d'une demande
                     m, t = mqhome.receive(type=2, block=False)
                     dem = m.decode()
                     pidm, quantitym = dem.split(",")
@@ -124,7 +137,7 @@ if __name__ == "__main__":
         p = multiprocessing.Process(target=maison, args=(InitProd, ConsoRate, SalePol, mqhome, mqmarket))
         p.start()
 
-        # Mettre la barrière dans une boucle et faire le print Tour suivant !
+
 """
     while True:
 
